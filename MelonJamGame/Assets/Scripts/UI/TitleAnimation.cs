9e600deace34e7dd1ea8289b286dcfd06541b3d9
Jadden Picardal <jaddenki@gmail.com>
2023-12-28 21:36:24 -0800

    Fixed Title and Intro Scene Animations

---
+++ MelonJamGame/Assets/Scripts/UI/TitleAnimation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleAnimation : MonoBehaviour
+{
+    public Sprite[] Idle;
+
+    public float frameInterval = 0.2f; // Time interval between frames
+
+    private SpriteRenderer spriteRenderer;
+    private int currentFrameIndex = 0;
+    private int poo;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        InvokeRepeating("NextFrame", 0f, frameInterval);
+    }
+
+    void NextFrame()
+    {
+
+
+            spriteRenderer.sprite = Idle[currentFrameIndex];
+            poo = 2;
+
+        // Move to the next frame index
+        currentFrameIndex = (currentFrameIndex + 1) % poo;
+    }
+}
